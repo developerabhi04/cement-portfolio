6c285b913a43375dcc771cb75f8df61ae347b583
stark <developerabhi04@gmail.com>
2024-11-30 05:28:20 +0530

    kk

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -5,13 +5,14 @@ import AboutUs from "../pages/AboutUs";
 import Services from "../pages/Services";
 import Solution from "../pages/Solution";
 import LiveCount from "../pages/LiveCount";
+import FormPopup from "../pages/FormPopup";
 
 
 const HomeLayout = () => {
 
     return (
         <>
-
+            <FormPopup />
             <Home />
             <Construction />
             <Form />
