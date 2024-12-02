682c1131d16292730220df557bf08ca9d8c30770
stark <developerabhi04@gmail.com>
2024-12-02 17:38:05 +0530

    h

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./styles/app.scss";
 import App from './App.jsx';
 
 
-export const server = "https://674ceabbb97b1424066d4ff1--adanicement-backend.netlify.app/api/v1";
+export const server = "https://adanicement-backend.netlify.app/api/v1";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
