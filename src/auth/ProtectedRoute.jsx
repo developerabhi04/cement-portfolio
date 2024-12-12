b01e0a35b17085dd7b607f7cfdfc517ceff5e97c
stark <developerabhi04@gmail.com>
2024-12-13 00:48:01 +0530

    jj

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
+import Cookies from "js-cookie"; // You need to install js-cookie
 
 const ProtectedRoute = ({ children, redirect = "/admin" }) => {
     // Check for token in localStorage or cookies
-    const token = localStorage.getItem("Admin-Token");
+    const token = localStorage.getItem("Admin-Token") || Cookies.get("Admin-Token");
 
     if (!token) return <Navigate to={redirect} />;
 
